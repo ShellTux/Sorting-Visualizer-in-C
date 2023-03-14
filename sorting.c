899afec5aa01156243b63c3ce816811828bf527b
ShellTux <eu@exemplo.com>
2023-03-14 11:21:55 +0000

    Resolved warning comparison of 2 variables of different type
    sorting.c: In function ‘shuffleArray’:
    sorting.c:10:31: warning: comparison of integer expressions of different signedness: ‘size_t’ {aka ‘long unsigned int’} and ‘int’ [-Wsign-compare]
       10 |                 for (i = 0; i < size - 1; i++) {
          |                               ^

--- sorting.c
+++ sorting.c
@@ -7,7 +7,8 @@ typedef unsigned int u32 ;
 void shuffleArray(u32 array[], int size) {
 	if (size > 1) {
 		size_t i;
-		for (i = 0; i < size - 1; i++) {
+		size_t arrayLength = size;
+		for (i = 0; i < arrayLength - 1; i++) {
 			size_t j = i + rand() / (RAND_MAX / (size - i) + 1);
 			u32 t = array[j];
 			array[j] = array[i];
