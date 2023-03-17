5f3a43326557674ba319400a0bf3202135e21731
ShellTux <eu@exemplo.com>
2023-03-17 16:57:33 +0000

    Shuffle array with orange color and add rendering for quick sort

--- sorting.c
+++ sorting.c
@@ -21,9 +21,9 @@ void shuffleArray(int array[], int arrayLength) {
 			int j = i + rand() / (RAND_MAX / (arrayLength - i) + 1);
 			swap(array, i, j);
 
-			/* int highlighting[] = { i , j }; */
-			/* drawBars(renderer, array, arrayLength, BAR_R, BAR_G, BAR_B, highlighting, 2, HIGHLIGHT_R, HIGHLIGHT_G, HIGHLIGHT_B); */
-			/* SDL_Delay(50); */
+			int highlighting[] = { i , j };
+			drawBars(renderer, array, arrayLength, BAR_R, BAR_G, BAR_B, highlighting, 2, 255, 152, 3);
+			SDL_Delay(SHUFFLE_DELAY);
 		}
 	}
 }; 
@@ -38,31 +38,45 @@ void initializeArray(int array[], int arrayLength, int minimum, int maximum) {
 /* Quick Sorting START */
 /* ################### */
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high, int length) {
 	int pivot = arr[high]; // pivot
 	int i = (low - 1); // Index of smaller element and indicates
 				// the right position of pivot found so far
 
+	int highlighting[2];
 	for (int j = low; j <= high - 1; j++) {
 		// If current element is smaller than the pivot
 		if (arr[j] < pivot) {
 			i++; // increment index of smaller element
 			swap(arr, i, j);
+			highlighting[0] = i;
+			highlighting[1] = j;
+			drawBars(renderer, arr, length, BAR_R, BAR_G, BAR_B, highlighting, 2, HIGHLIGHT_R, HIGHLIGHT_G, HIGHLIGHT_B);
+			SDL_Delay(SHUFFLE_DELAY);
 		}
 	}
+	highlighting[0] = i + 1;
+	highlighting[1] = high;
 	swap(arr, i + 1, high);
+	drawBars(renderer, arr, length, BAR_R, BAR_G, BAR_B, highlighting, 2, HIGHLIGHT_R, HIGHLIGHT_G, HIGHLIGHT_B);
+	SDL_Delay(SHUFFLE_DELAY);
 	return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high) {
+void quickSort(int arr[], int low, int high, int length) {
 	if (low < high) {
 		/* pi is partitioning index, arr[p] is now
 		at right place */
-		int pi = partition(arr, low, high);
+		int pi = partition(arr, low, high, length);
 
 		// Separately sort elements before
 		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
+		quickSort(arr, low, pi - 1, length);
+		quickSort(arr, pi + 1, high, length);
 	}
 }
+
+/* ################# */
+/* Quick Sorting END */
+/* ################# */
+
