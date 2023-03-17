32a80fdad402d9955d67558df40d640b18e029bf
ShellTux <eu@exemplo.com>
2023-03-17 16:58:23 +0000

    Added quickSort declaration on sorting.h

--- sorting.h
+++ sorting.h
@@ -1,2 +1,3 @@
 void shuffleArray(int array[], int arrayLength);
 void initializeArray(int array[], int arrayLength, int minimum, int maximum);
+void quickSort(int arr[], int low, int high, int length);
